5ddd05b04d20d3cfd069b7dfb80acbc90b03f3b5
EC2 Default User <ec2-user@ip-172-31-28-39.eu-west-2.compute.internal>
2020-05-01 18:29:21 +0000

    added public subnets

--- 6-subnt.tf
+++ 6-subnt.tf
@@ -1,6 +1,13 @@
-resource "aws_subnet" "myperfect_vpc" {
+resource "aws_subnet" "main" {
     count = 3
     vpc_id = "${aws_vpc.myperfect_vpc.id}"
     cidr_block = "10.0.${count.index+1}.0/24"
+  
+}
+# public subnets
+resource "aws_subnet" "public" {
+    count = 3
+    vpc_id = "${aws_vpc.myperfect_vpc.id}"
+    cidr_block = "10.0.10${count.index+1}.0/24"
   
 }
