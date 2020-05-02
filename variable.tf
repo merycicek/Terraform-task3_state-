5f11b1f2f74cd7289229e30d500ec5f7a5160cc8
EC2 Default User <ec2-user@ip-172-31-28-39.eu-west-2.compute.internal>
2020-05-02 22:37:45 +0000

    added soft codes

--- variable.tf
+++ variable.tf
@@ -1,2 +1,12 @@
 variable "vpc_cidr" {}
 
+variable "public_cidr1" {}
+variable "public_cidr2" {}
+variable "public_cidr3" {}
+variable "tags" {
+    type = "map"
+}
+
+variable "public_cidr1" {}
+variable "public_cidr2" {}
+variable "public_cidr3" {}
