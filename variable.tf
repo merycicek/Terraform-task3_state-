cbe7196bc6eae14daf27c9d37c30112e552266dd
EC2 Default User <ec2-user@ip-172-31-28-39.eu-west-2.compute.internal>
2020-05-03 00:00:58 +0000

    added variables

--- variable.tf
+++ variable.tf
@@ -3,11 +3,35 @@ variable "vpc_cidr" {}
 variable "private_cidr1" {}
 variable "private_cidr2" {}
 variable "private_cidr3" {}
-variable "tags" {
-    type = "map"
-}
+
 
 variable "public_cidr1" {}
 variable "public_cidr2" {}
 variable "public_cidr3" {}
+
+
+variable "region" {}
+variable "region_name" {}
+variable "az1" {}
+variable "az2" {}
+variable "az3" {}
+  
+  variable "tags" {
+    type = "map"
+}
+
+
+
+  
+
+
+  
+
+
+  
+
+
+  
+
+
 
